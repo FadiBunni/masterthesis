5596e3dbe3b20ac0115bc30b667b1558ecb5e75f
Fadi Bunni <fadibunni_guitar@hotmail.com>
2022-11-18 16:27:20 +0100

    added keys

--- PowerMeasurementEnd-Node/keyfiles/lorawan-keys_example.h
+++ PowerMeasurementEnd-Node/keyfiles/lorawan-keys_example.h
@@ -65,3 +65,77 @@
 
 
 #endif  // LORAWAN_KEYS_H_
+
+
+
+/*******************************************************************************
+ *
+ *  File:          lorawan-keys_example.h
+ * 
+ *  Function:      Example for lorawan-keys.h required by LMIC-node.
+ *
+ *  Copyright:     Copyright (c) 2021 Leonel Lopes Parente
+ *
+ *  Important      ██ DO NOT EDIT THIS EXAMPLE FILE (see instructions below) ██
+ * 
+ *  Decription:    lorawan-keys.h defines LoRaWAN keys needed by the LMIC library.
+ *                 It can contain keys for both OTAA and for ABP activation.
+ *                 Only the keys for the used activation type need to be specified.
+ * 
+ *                 It is essential that each key is specified in the correct format.
+ *                 lsb: least-significant-byte first, msb: most-significant-byte first.
+ * 
+ *                 For security reasons all files in the keyfiles folder (except file
+ *                 lorawan-keys_example.h) are excluded from the Git(Hub) repository.
+ *                 Also excluded are all files matching the pattern *lorawan-keys.h.
+ *                 This way they cannot be accidentally committed to a public repository.
+ * 
+ *  Instructions:  1. Copy this file lorawan-keys_example.h to file lorawan-keys.h
+ *                    in the same folder (keyfiles).
+ *                 2. Place/edit required LoRaWAN keys in the new lorawan-keys.h file.
+ *
+ ******************************************************************************/
+
+#pragma once
+
+#ifndef LORAWAN_KEYS_H_
+#define LORAWAN_KEYS_H_
+
+// Optional: If DEVICEID is defined it will be used instead of the default defined in the BSF.
+// #define DEVICEID "<deviceid>"
+
+// Keys required for OTAA activation:
+
+// End-device Identifier (u1_t[8]) in lsb format
+//#define OTAA_DEVEUI 0x25, 0xE6, 0x04, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 //TTN
+//#define OTAA_DEVEUI 0x25, 0xE6, 0x04, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 //Chirpstack logger
+#define OTAA_DEVEUI 0x20, 0x30, 0x29, 0x0e, 0xe6, 0x94, 0x96, 0x41 //Chirpstack kasper
+
+// Application Identifier (u1_t[8]) in lsb format
+//#define OTAA_APPEUI 0x52, 0x20, 0x56, 0x83, 0x52, 0x82, 0x11, 0x14 //TTN
+#define OTAA_APPEUI 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 //Chirpstack
+
+// Application Key (u1_t[16]) in msb format
+//#define OTAA_APPKEY 0x0E, 0xFA, 0xB8, 0xD0, 0x17, 0xB4, 0xA7, 0xB8, 0xEC, 0xA3, 0xBD, 0x63, 0x97, 0xED, 0x63, 0x47 //TNN
+//#define OTAA_APPKEY 0xa0, 0x6b, 0x01, 0xe0, 0x03, 0x8f, 0x54, 0xa4, 0xb9, 0x12, 0xc2, 0x00, 0xdf, 0xd0, 0xf8, 0x3c //Chirpstack Logger
+#define OTAA_APPKEY 0x57, 0x40, 0x7e, 0x20, 0x12, 0x1c, 0x32, 0x0f, 0xcc, 0xf5, 0x5c, 0x17, 0x7a, 0x30, 0x04, 0x25 //Chirpstack Kasper
+
+// -----------------------------------------------------------------------------
+
+// Optional: If ABP_DEVICEID is defined it will be used for ABP instead of the default defined in the BSF.
+// #define ABP_DEVICEID "<deviceid>"
+
+// Keys required for ABP activation:
+
+// End-device Address (u4_t) in uint32_t format. 
+// Note: The value must start with 0x (current version of TTN Console does not provide this).
+#define ABP_DEVADDR 0x00000000
+
+// Network Session Key (u1_t[16]) in msb format
+#define ABP_NWKSKEY 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+// Application Session K (u1_t[16]) in msb format
+#define ABP_APPSKEY 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+
+#endif  // LORAWAN_KEYS_H_
